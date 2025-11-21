b8121181da911241ca6fc805bbf3cd3581292850
yhtttt050723 <519275808@qq.com>
2025-11-21 20:41:11 +0800

    Login页面

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -1,12 +1,17 @@
-import {createRouter,createWebHistory}  from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
+import Login from '../views/Login.vue'
 
 const routes = [
-
+  {
+    path: '/',
+    name: 'login',
+    component: Login
+  }
 ]
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
-    routes:routes
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes
 })
 
 export default router
